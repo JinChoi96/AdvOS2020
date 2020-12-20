c856e6fa58544230415cd2205d19621fb2b0c156
jinchoi96 <babjinny@naver.com>
2020-12-20 21:02:08 +0900

    change iteration

--- hybrid_sched/dummywl.cpp
+++ hybrid_sched/dummywl.cpp
@@ -1,41 +1,50 @@
 #include "dummywl.h"
 
 void *dummy_work(void *data){
-    if(HYBRID){
-        Task *task = (Task*)data;
-        struct sched_attr attr = task->get_attr();
-        int ret;
-        unsigned int flags = 0;
+    Task *task = (Task*)data;
+    struct sched_attr attr = task->get_attr();
+    int ret;
+    unsigned int flags = 0;
 
-        ret = task->sched_setattr(0, &attr, flags);
-        if(ret < 0){
-            perror("sched_setattr");
-            exit(-1);
+    ret = task->sched_setattr(0, &attr, flags);
+    if(ret < 0){
+        perror("sched_setattr");
+        exit(-1);
+    }
+
+    // iter * k(=6)
+    int iter = 500000000;
+    int sum = 0;
+
+    auto start = std::chrono::high_resolution_clock::now();  
+
+    for(int k = 0 ; k < 6; k++){  
+        for(unsigned i = 0; i < iter; i++){
+            sum += i;
         }
     }
-    int iter = 10000000;
-    int sum = 0;
-    
-    for(unsigned i = 0; i < iter; i++){
-        sum += i;
-    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::milliseconds elapsedNS = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);    
+    std::cout<<"time: "<< elapsedNS.count()<<" ms\n";    
+
 }
 
+
 void *dummy_write(void *data){
-    if(HYBRID){
-        Task *task = (Task*)data;
-        struct sched_attr attr = task->get_attr();
-        int ret;
-        unsigned int flags = 0;
+    Task *task = (Task*)data;
+    struct sched_attr attr = task->get_attr();
+    int ret;
+    unsigned int flags = 0;
 
-        ret = task->sched_setattr(0, &attr, flags);
-        if(ret < 0){
-            perror("sched_setattr");
-            exit(-1);
-        }
+    ret = task->sched_setattr(0, &attr, flags);
+    if(ret < 0){
+        perror("sched_setattr");
+        exit(-1);
     }
-    int iter = 10000;
 
+    int iter = 1000000;
+
+    auto start = std::chrono::high_resolution_clock::now();
     for(int i = 0; i < iter; i++){
         std::fstream dummyfile ("dummytext.txt");
         if (dummyfile.is_open())
@@ -47,31 +56,35 @@ void *dummy_write(void *data){
             std::cout << "Unable to open file";
         }
     }
+    auto end = std::chrono::high_resolution_clock::now();   
+    std::chrono::milliseconds elapsedNS = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);    
+    std::cout<<"time: "<< elapsedNS.count()<<" ms\n"; 
+
 }
 
 void *dummy_read(void *data){
-    if(HYBRID){
-        Task *task = (Task*)data;
-        struct sched_attr attr = task->get_attr();
-        int ret;
-        unsigned int flags = 0;
+    Task *task = (Task*)data;
+    struct sched_attr attr = task->get_attr();
+    int ret;
+    unsigned int flags = 0;
 
-        ret = task->sched_setattr(0, &attr, flags);
-        if(ret < 0){
-            perror("sched_setattr");
-            exit(-1);
-        }
+    ret = task->sched_setattr(0, &attr, flags);
+    if(ret < 0){
+        perror("sched_setattr");
+        exit(-1);
     }
-    std::string line;
-    int iter = 10000;
 
+    std::string line;
+    int iter = 1000000;
+
+    auto start = std::chrono::high_resolution_clock::now();
     for(int i = 0; i < iter; i++){
         std::fstream dummyfile ("dummytext.txt");
         
         if (dummyfile.is_open())
         {        
             while(std::getline(dummyfile,line)){
-                //std::cout << line << '\n';
+                // std::cout << line << '\n';
             }      
             dummyfile.close();
         }
@@ -79,22 +92,25 @@ void *dummy_read(void *data){
             std::cout << "Unable to open file";
         }
     }
+    auto end = std::chrono::high_resolution_clock::now();   
+    std::chrono::milliseconds elapsedNS = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);    
+    std::cout<<"time: "<< elapsedNS.count()<<" ms\n"; 
+
 }
 
 void *dummy_sort(void *data){
-    if(HYBRID){
-        Task *task = (Task*)data;
-        struct sched_attr attr = task->get_attr();
-        int ret;
-        unsigned int flags = 0;
+    Task *task = (Task*)data;
+    struct sched_attr attr = task->get_attr();
+    int ret;
+    unsigned int flags = 0;
 
-        ret = task->sched_setattr(0, &attr, flags);
-        if(ret < 0){
-            perror("sched_setattr");
-            exit(-1);
-        }
+    ret = task->sched_setattr(0, &attr, flags);
+    if(ret < 0){
+        perror("sched_setattr");
+        exit(-1);
     }
-    int iter = 10000;
+
+    int iter = 30000;
 
     int size = 1000;
     std::random_device rd;
@@ -102,10 +118,15 @@ void *dummy_sort(void *data){
     std::uniform_int_distribution<int> dis(0,1000);
     int iarr[size] = {0};
 
+    auto start = std::chrono::high_resolution_clock::now();
     for(int i = 0; i < iter; i++){
         for(int j = 0; j < size; j++){
             iarr[j] = dis(gen);
         }
         std::sort(iarr,iarr+size);
-    }   
+    }  
+    auto end = std::chrono::high_resolution_clock::now();    
+    std::chrono::milliseconds elapsedNS = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);    
+    std::cout<<"time: "<< elapsedNS.count()<<" ms\n";
+ 
 }
